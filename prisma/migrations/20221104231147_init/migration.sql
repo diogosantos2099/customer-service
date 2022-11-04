e389a16380f2fec8b581b5533de7599868205fb6
DiogoS2099 <diogo.miguel.c.santos@gmail.com>
2022-11-04 23:27:05 +0000

    Generated Prisma Client and migration script. Created seed to populate db.

---
+++ prisma/migrations/20221104231147_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
