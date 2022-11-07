e10526c49f7441d0c0e0f4bdaf32112bde2f67f2
DiogoS2099 <diogo.miguel.c.santos@gmail.com>
2022-11-07 20:48:14 +0000

    Refactored e2e tests. Only testing the endpoints that do not change DB data

--- test/customer.e2e-spec.ts
+++ test/customer.e2e-spec.ts
@@ -1,11 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
-import { Customer } from '../src/customer/entities/customer.entity';
 
-describe('AppController (e2e)', () => {
+/**
+ * These e2e tests use the real DB.
+ * In order for them to pass, the DB needs to have data.
+ * See README.md, section 'Database Setup' for details.
+ */
+describe('CustomerController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
@@ -22,12 +26,13 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/customer/{id} (GET)', () => {
+  it('/customer (GET) should return 200', () => {
+    return request(app.getHttpServer()).get('/customer').expect(200);
+  });
+
+  it('/customer/{id} (GET) should return 200', () => {
     const id = 1;
-    const response: Customer = { id, name: 'customer name' };
-    return request(app.getHttpServer())
-      .get(`/customer/${id}`)
-      .expect(200)
-      .expect(response);
+
+    return request(app.getHttpServer()).get(`/customer/${id}`).expect(200);
   });
 });
